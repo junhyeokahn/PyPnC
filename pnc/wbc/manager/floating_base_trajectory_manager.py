fix quaternion flipping issue, now balancing working in pydart but not in pybullet

pnc/wbc/manager/floating_base_trajectory_manager.py
@@ -28,8 +28,9 @@ class FloatingBaseTrajectoryManager(object):
         self._ini_base_quat = util.rot_to_quat(
             self._robot.get_link_iso(self._base_ori_task.target_id)[0:3, 0:3])
         self._target_base_quat = target_base_quat
-        self._quat_error = (R.from_quat(self._target_base_quat) *
-                            R.from_quat(self._ini_base_quat).inv()).as_quat()
+        self._quat_error = R.from_matrix(np.dot(
+            R.from_quat(self._target_base_quat).as_matrix(),
+            R.from_quat(self._ini_base_quat).as_matrix().transpose())).as_quat()
         self._exp_error = util.quat_to_exp(self._quat_error)
 
         ## TODO : Check this with dart function
@@ -73,8 +74,8 @@ class FloatingBaseTrajectoryManager(object):
         # print(quat_inc, quat_inc_)
         # __import__('ipdb').set_trace()
         ##
-        base_quat_des = (R.from_quat(quat_inc) *
-                         R.from_quat(self._ini_base_quat)).as_quat()
+        base_quat_des = R.from_matrix(np.dot(R.from_quat(quat_inc).as_matrix(),
+                         R.from_quat(self._ini_base_quat).as_matrix())).as_quat()
         base_angvel_des = self._exp_error * scaled_tdot
         base_angacc_des = self._exp_error * scaled_tddot
 