8e6d95abd28cc1f690441579dd9071575cb9ddc4
junhyeokahn <junhyeokahn91@gmail.com>
2020-12-30 22:46:52 -0600

    add final foot nodes cost

--- pnc/planner/locomotion/towr_plus/include/towr_plus/constraints/swing_constraint.h
+++ pnc/planner/locomotion/towr_plus/include/towr_plus/constraints/swing_constraint.h
@@ -68,7 +68,8 @@ public:
 
 private:
   NodesVariablesPhaseBased::Ptr ee_motion_;
-  double t_swing_avg_ = 0.3;
+  // double t_swing_avg_ = 0.3;
+  double t_swing_avg_ = 0.75;
   std::string ee_motion_id_;
 
   std::vector<int> pure_swing_node_ids_;
