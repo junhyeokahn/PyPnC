cd803a4f31643f9caadc978511e6a8a07c7b2442
junhyeokahn <junhyeokahn91@gmail.com>
2021-11-05 10:41:30 -0500

    working on laikago

--- simulator/pybullet/laikago_main.py
+++ simulator/pybullet/laikago_main.py
@@ -3,116 +3,76 @@ import sys
 cwd = os.getcwd()
 sys.path.append(cwd)
 
-import pybullet as p
 import time
+
+import pybullet as p
+import numpy as np
+
+from util import pybullet_util
 
 p.connect(p.GUI)
 plane = p.loadURDF(cwd + "/robot_model/ground/plane.urdf")
 p.setGravity(0, 0, -9.8)
-p.setTimeStep(1. / 500)
-#p.setDefaultContactERP(0)
-#urdfFlags = p.URDF_USE_SELF_COLLISION+p.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS
-urdfFlags = p.URDF_USE_SELF_COLLISION
+dt = 0.002
+p.setTimeStep(dt)
+
+ini_pos = [0., 0., 0.5]
+ini_quat = [0.5, 0.5, 0.5, 0.5]
+
 quadruped = p.loadURDF(cwd + "/robot_model/laikago/laikago_toes.urdf",
-                       [0, 0, .5], [0, 0.5, 0.5, 0],
-                       flags=urdfFlags,
+                       ini_pos,
+                       ini_quat,
                        useFixedBase=False)
 
-#enable collision between lower legs
+nq, nv, na, joint_id, link_id, pos_basejoint_to_basecom, rot_basejoint_to_basecom = pybullet_util.get_robot_config(
+    quadruped, ini_pos, ini_quat, True)
 
-for j in range(p.getNumJoints(quadruped)):
-    print(p.getJointInfo(quadruped, j))
+p.resetJointState(quadruped, joint_id["FR_lower_leg_2_upper_leg_joint"], -0.56,
+                  0.)
+p.resetJointState(quadruped, joint_id["FL_lower_leg_2_upper_leg_joint"], -0.56,
+                  0.)
+p.resetJointState(quadruped, joint_id["RR_lower_leg_2_upper_leg_joint"], -0.56,
+                  0.)
+p.resetJointState(quadruped, joint_id["RL_lower_leg_2_upper_leg_joint"], -0.56,
+                  0.)
 
-#2,5,8 and 11 are the lower legs
-lower_legs = [2, 5, 8, 11]
-for l0 in lower_legs:
-    for l1 in lower_legs:
-        if (l1 > l0):
-            enableCollision = 1
-            print("collision for pair", l0, l1,
-                  p.getJointInfo(quadruped, l0)[12],
-                  p.getJointInfo(quadruped, l1)[12], "enabled=",
-                  enableCollision)
-            p.setCollisionFilterPair(quadruped, quadruped, l0, l1,
-                                     enableCollision)
+pybullet_util.set_link_damping(quadruped, link_id.values(), 0., 0.)
+pybullet_util.set_joint_friction(quadruped, joint_id, 0)
 
-jointIds = []
-paramIds = []
-jointOffsets = []
-jointDirections = [-1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1]
-jointAngles = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+nominal_sensor_data = pybullet_util.get_sensor_data(quadruped, joint_id,
+                                                    link_id,
+                                                    pos_basejoint_to_basecom,
+                                                    rot_basejoint_to_basecom)
 
-for i in range(4):
-    jointOffsets.append(0)
-    jointOffsets.append(-0.7)
-    jointOffsets.append(0.7)
-
-maxForceId = p.addUserDebugParameter("maxForce", 0, 100, 20)
-
-for j in range(p.getNumJoints(quadruped)):
-    p.changeDynamics(quadruped, j, linearDamping=0, angularDamping=0)
-    info = p.getJointInfo(quadruped, j)
-    #print(info)
-    jointName = info[1]
-    jointType = info[2]
-    if (jointType == p.JOINT_PRISMATIC or jointType == p.JOINT_REVOLUTE):
-        jointIds.append(j)
-
-p.getCameraImage(480, 320)
-p.setRealTimeSimulation(0)
-
-joints = []
-
-with open(cwd + "/../pybullet_robots/data1.txt", "r") as filestream:
-    for line in filestream:
-        maxForce = p.readUserDebugParameter(maxForceId)
-        currentline = line.split(",")
-        frame = currentline[0]
-        t = currentline[1]
-        joints = currentline[2:14]
-        for j in range(12):
-            targetPos = float(joints[j])
-            p.setJointMotorControl2(quadruped,
-                                    jointIds[j],
-                                    p.POSITION_CONTROL,
-                                    jointDirections[j] * targetPos +
-                                    jointOffsets[j],
-                                    force=maxForce)
-        p.stepSimulation()
-        for lower_leg in lower_legs:
-            #print("points for ", quadruped, " link: ", lower_leg)
-            pts = p.getContactPoints(quadruped, -1, lower_leg)
-            #print("num points=",len(pts))
-            #for pt in pts:
-            #	print(pt[9])
-        time.sleep(1. / 500.)
-
-index = 0
-for j in range(p.getNumJoints(quadruped)):
-    p.changeDynamics(quadruped, j, linearDamping=0, angularDamping=0)
-    info = p.getJointInfo(quadruped, j)
-    js = p.getJointState(quadruped, j)
-    #print(info)
-    jointName = info[1]
-    jointType = info[2]
-    if (jointType == p.JOINT_PRISMATIC or jointType == p.JOINT_REVOLUTE):
-        paramIds.append(
-            p.addUserDebugParameter(jointName.decode("utf-8"), -4, 4,
-                                    (js[0] - jointOffsets[index]) /
-                                    jointDirections[index]))
-        index = index + 1
-
-p.setRealTimeSimulation(1)
+t = 0
+count = 0
 
 while (1):
 
-    for i in range(len(paramIds)):
-        c = paramIds[i]
-        targetPos = p.readUserDebugParameter(c)
-        maxForce = p.readUserDebugParameter(maxForceId)
-        p.setJointMotorControl2(quadruped,
-                                jointIds[i],
-                                p.POSITION_CONTROL,
-                                jointDirections[i] * targetPos +
-                                jointOffsets[i],
-                                force=maxForce)
+    # Get sensor data
+    sensor_data = pybullet_util.get_sensor_data(quadruped, joint_id, link_id,
+                                                pos_basejoint_to_basecom,
+                                                rot_basejoint_to_basecom)
+
+    # Set motor cmd
+    pybullet_util.set_motor_pos(quadruped, joint_id,
+                                nominal_sensor_data['joint_pos'])
+
+    p.stepSimulation()
+    t += dt
+    count += 1
+    time.sleep(dt)
+
+    print("-" * 80)
+    print("toeFR")
+    print(pybullet_util.get_link_iso(quadruped, link_id['toeFR']))
+    print("toeFL")
+    print(pybullet_util.get_link_iso(quadruped, link_id['toeFL']))
+    print("toeRR")
+    print(pybullet_util.get_link_iso(quadruped, link_id['toeRR']))
+    print("toeRL")
+    print(pybullet_util.get_link_iso(quadruped, link_id['toeRL']))
+    print("base pos")
+    print(sensor_data["base_com_pos"])
+    print("base quat")
+    print(sensor_data["base_com_quat"])
