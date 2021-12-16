1a038ac282e0e8cf19a7af04928271ed00b61407
junhyeokahn <junhyeokahn91@gmail.com>
2021-12-16 10:41:31 -0600

    gripper test done

--- test/grasping.py
+++ test/grasping.py
@@ -46,17 +46,25 @@ p.loadURDF(cwd + "/robot_model/bookcase/blue_can.urdf",
            useFixedBase=0,
            basePosition=[0 + xOffset, 0, 0.7])
 
+# gripper = p.loadURDF(cwd + "/robot_model/ezgripper/gripper.urdf",
+# useFixedBase=1,
+# basePosition=[0 + xOffset - 0.2, 0, 0.7])
 gripper = p.loadURDF(cwd + "/robot_model/ezgripper/gripper.urdf",
-                     useFixedBase=0,
-                     basePosition=[0, 0, 1])
+                     useFixedBase=1,
+                     basePosition=[0 + xOffset - 0.2, -0.7, 1.35])
 
 gripper_joints = [
     "left_ezgripper_knuckle_palm_L1_1", "left_ezgripper_knuckle_L1_L2_1",
     "left_ezgripper_knuckle_palm_L1_2", "left_ezgripper_knuckle_L1_L2_2"
 ]
 
+gripper_floating = {"basePosX": 0, "basePosY": 1, "basePosZ": 2}
+
 nq, nv, na, joint_id, link_id, pos_basejoint_to_basecom, rot_basejoint_to_basecom = pybullet_util.get_robot_config(
-    gripper, [0, 0, 1], [0, 0, 0, 1], False)
+    gripper, [0, 0, 1], [0, 0, 0, 1], True)
+
+pybullet_util.set_joint_friction(gripper, gripper_floating, 0.)
+pybullet_util.set_link_damping(gripper, link_id.values(), 0., 0.)
 
 nominal_sensor_data = pybullet_util.get_sensor_data(gripper, joint_id, link_id,
                                                     pos_basejoint_to_basecom,
@@ -70,38 +78,50 @@ for gripper_joint in gripper_joints:
     gripper_command[gripper_joint] = nominal_sensor_data['joint_pos'][
         gripper_joint]
 
+for gripper_fjoint in gripper_floating.keys():
+    gripper_command[gripper_fjoint] = nominal_sensor_data['joint_pos'][
+        gripper_fjoint]
+
 #GRIPPER_DELTA_ANGLE = 1.94 / 3
 GRIPPER_DELTA_ANGLE = 1.94 / 4
 
 while (1):
 
-    p.resetBasePositionAndOrientation(gripper, gripper_pos, gripper_quat)
-
-
     keys = p.getKeyboardEvents()
     if pybullet_util.is_key_triggered(keys, 'c'):
-        for k, v in gripper_command.items():
+        for k in gripper_joints:
             gripper_command[k] += GRIPPER_DELTA_ANGLE
     elif pybullet_util.is_key_triggered(keys, 'o'):
-        for k, v in gripper_command.items():
+        for k in gripper_joints:
             gripper_command[k] -= GRIPPER_DELTA_ANGLE
     elif pybullet_util.is_key_triggered(keys, 'u'):
-        gripper_pos[2] += 0.02  # z
+        # gripper_pos[2] += 0.02  # z
+        gripper_command['basePosZ'] += 0.02
     elif pybullet_util.is_key_triggered(keys, 'd'):
-        gripper_pos[2] -= 0.02  # z
+        # gripper_pos[2] -= 0.02  # z
+        gripper_command['basePosZ'] -= 0.02
     elif pybullet_util.is_key_triggered(keys, 'f'):
-        gripper_pos[0] += 0.02  # x
+        # gripper_pos[0] += 0.02  # x
+        gripper_command['basePosX'] += 0.02
     elif pybullet_util.is_key_triggered(keys, 'b'):
-        gripper_pos[0] -= 0.02  # x
+        # gripper_pos[0] -= 0.02  # x
+        gripper_command['basePosX'] -= 0.02
     elif pybullet_util.is_key_triggered(keys, 'l'):
-        gripper_pos[1] += 0.02  # y
+        # gripper_pos[1] += 0.02  # y
+        gripper_command['basePosY'] += 0.02
     elif pybullet_util.is_key_triggered(keys, 'r'):
-        gripper_pos[1] -= 0.02  # y
+        # gripper_pos[1] -= 0.02  # y
+        gripper_command['basePosY'] -= 0.02
     elif pybullet_util.is_key_triggered(keys, 's'):
-        gripper_pos[0] = 0.82  # x
-        gripper_pos[1] = 0.0  # y
-        gripper_pos[2] = 0.719  # z
-    pybullet_util.set_motor_pos(gripper, joint_id, gripper_command)
+        # gripper_pos[0] = 0.82  # x
+        # gripper_pos[1] = 0.0  # y
+        # gripper_pos[2] = 0.719  # z
+        gripper_command['basePosX'] = 0.82  # x
+        gripper_command['basePosY'] = 0.0  # y
+        gripper_command['basePosZ'] = 0.019  # z
 
+    # print("-" * 80)
+    # print(gripper_command)
+    pybullet_util.set_motor_pos(gripper, joint_id, gripper_command)
 
     p.stepSimulation()
