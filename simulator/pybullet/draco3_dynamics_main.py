551cc272408978e44d00e07964110d4e748d9db4
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2022-08-22 10:22:39 -0500

    load updated Draco urdf

--- simulator/pybullet/draco3_dynamics_main.py
+++ simulator/pybullet/draco3_dynamics_main.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     # Create Robot, Ground
     p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
     # robot = p.loadURDF(cwd + "/robot_model/draco3/draco3.urdf",
-    robot = p.loadURDF(cwd + "/robot_model/draco3/draco3_gripper.urdf",
+    robot = p.loadURDF(cwd + "/robot_model/draco3/draco3_gripper_mesh_updated.urdf",
                        SimConfig.INITIAL_POS_WORLD_TO_BASEJOINT,
                        SimConfig.INITIAL_QUAT_WORLD_TO_BASEJOINT)
 
