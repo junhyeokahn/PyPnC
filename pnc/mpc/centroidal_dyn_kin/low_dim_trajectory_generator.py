a782b21e19399b1a0b9f3bafd65a4bc87354704c
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2022-09-05 02:59:14 -0500

    fixed desired x- and y-CoM in generation of squatting trajectory

--- pnc/mpc/centroidal_dyn_kin/low_dim_trajectory_generator.py
+++ pnc/mpc/centroidal_dyn_kin/low_dim_trajectory_generator.py
@@ -13,13 +13,12 @@ def get_desired_mpc_trajectory(task, s0, u_guess, model, N_horizon, w=1, A=0.0):
     na = model.na
     dt = model.dt
 
-    s_traj = np.zeros((ns, N_horizon))
+    s_traj = np.repeat(np.reshape(s0, [ns, 1]), N_horizon + 1, axis=1)
     if task == Task.STAND:
-        s_traj = np.repeat(np.reshape(s0, [ns, 1]), N_horizon+1, axis=1)
+        pass
     elif task == Task.SQUAT:
-        s_traj[:, 0] = s0
         # change desired CoM-z position and velocity entries
-        for n in range(N_horizon):
+        for n in range(N_horizon+1):
             s_traj[2, n] = s0[2] + A*np.sin(w * n*dt)
             s_traj[5, n] = s0[5] + A*np.cos(w * n*dt)
     u_guess_traj = np.repeat(np.reshape(u_guess, [na, 1]), N_horizon, axis=1)
